be9c746bb731ec0dd884843251530452d27abfa1
kyorohiro <kyorohiro@gmail.com>
2017-12-11 01:57:42 +0900

    add 09

--- babel01/src/index.js
+++ babel01/src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!!");
+class HelloWorld {
+    hello() {
+	console.log("Hello, World!!");
+    }
+}
+
+var helloWorld = new HelloWorld();
+helloWorld.hello();
+
+
+
--- babel01/webpack.config.js
+++ babel01/webpack.config.js
@@ -5,5 +5,14 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      include: path.resolve(__dirname, 'src'),
+      query: {
+        presets: ['es2015']
+      }
+    }]
+  },
 };
