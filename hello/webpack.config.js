8806a6cd2997abcffbf2941abe50685feeaa6ebf
kyorohiro <kyorohiro@gmail.com>
2017-12-12 07:56:11 +0900

    ..

---
+++ hello/webpack.config.js
@@ -0,0 +1,100 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+
+const base = {
+    devServer: {
+        contentBase: false,
+        host: '0.0.0.0',
+        port: process.env.PORT || 8361
+    },
+    devtool: 'cheap-module-source-map',
+    module: {
+        rules: [
+            {
+                include: [
+                    path.resolve(__dirname, 'src')
+                ],
+                test: /\.js$/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['es2015']
+                }
+            },
+            {
+                test: /node_modules[\\/](linebreak|grapheme-breaker)[\\/].*\.js$/,
+                loader: 'ify-loader'
+            }
+        ]
+    },
+    plugins: process.env.NODE_ENV === 'production' ? [
+        new webpack.optimize.UglifyJsPlugin({
+            include: /\.min\.js$/,
+            minimize: true
+        })
+    ] : []
+};
+
+module.exports = [
+    // Playground
+    Object.assign({}, base, {
+        target: 'web',
+        entry: {
+            'scratch-render': './src/index-web.js'
+        },
+        output: {
+            path: path.resolve(__dirname, 'playground'),
+            filename: '[name].js'
+        },
+        plugins: base.plugins.concat([
+            new CopyWebpackPlugin([
+                {
+                    from: 'src/playground'
+                }
+            ])
+        ])
+    }),
+    // Playground
+    Object.assign({}, base, {
+        target: 'web',
+        entry: {
+            'scratch-render': './src/index-web.js'
+        },
+        output: {
+            path: path.resolve(__dirname, 'hello'),
+            filename: '[name].js'
+        },
+        plugins: base.plugins.concat([
+            new CopyWebpackPlugin([
+                {
+                    from: 'src/hello'
+                }
+            ])
+        ])
+    }),
+    // Web-compatible
+    Object.assign({}, base, {
+        target: 'web',
+        entry: {
+            'scratch-render': './src/index-web.js',
+            'scratch-render.min': './src/index-web.js'
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist/web'),
+            filename: '[name].js'
+        }
+    }),
+    // Node-compatible
+    Object.assign({}, base, {
+        target: 'node',
+        entry: {
+            'scratch-render': './src/index.js'
+        },
+        output: {
+            library: 'ScratchRender',
+            libraryTarget: 'commonjs2',
+            path: path.resolve(__dirname, 'dist/node'),
+            filename: '[name].js'
+        }
+    })
+];
