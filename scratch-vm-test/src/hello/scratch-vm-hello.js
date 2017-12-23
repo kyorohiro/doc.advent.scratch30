9af7691145fb2141b7fd2690c5331039b92a2477
kyorohiro <kyorohiro@gmail.com>
2017-12-23 15:09:35 +0900

    ..

--- scratch-vm-test/src/hello/scratch-vm-hello.js
+++ scratch-vm-test/src/hello/scratch-vm-hello.js
@@ -14,46 +14,57 @@ const storage = new ScratchStorage();
 const audioEngine = new ScratchAudio();
 
 
-{
-	const ASSET_SERVER = 'https://cdn.assets.scratch.mit.edu/';
-	const PROJECT_SERVER = 'https://cdn.projects.scratch.mit.edu/';
-	const getProjectUrl = function (asset) {
-	    const assetIdParts = asset.assetId.split('.');
-	    const assetUrlParts = [PROJECT_SERVER, 'internalapi/project/', assetIdParts[0], '/get/'];
-	    if (assetIdParts[1]) {
-	        assetUrlParts.push(assetIdParts[1]);
-	    }
-	    return assetUrlParts.join('');
-	};
+//
+// Assets Setting
+//
+const ASSET_SERVER = 'https://cdn.assets.scratch.mit.edu/';
+const PROJECT_SERVER = 'https://cdn.projects.scratch.mit.edu/';
+const getProjectUrl = function (asset) {
+    const assetIdParts = asset.assetId.split('.');
+    const assetUrlParts = [PROJECT_SERVER, 'internalapi/project/', assetIdParts[0], '/get/'];
+    if (assetIdParts[1]) {
+        assetUrlParts.push(assetIdParts[1]);
+    }
+    return assetUrlParts.join('');
+};
 
-	const getAssetUrl = function (asset) {
-	    const assetUrlParts = [
-	        ASSET_SERVER,
-	        'internalapi/asset/',
-	        asset.assetId,
-	        '.',
-	        asset.dataFormat,
-	        '/get/'
-	    ];
-	    return assetUrlParts.join('');
-	};
-	storage.addWebSource([storage.AssetType.Project], getProjectUrl);
-	storage.addWebSource([storage.AssetType.ImageVector, storage.AssetType.ImageBitmap, storage.AssetType.Sound], getAssetUrl);
-	vm.attachStorage(storage);
-}
+const getAssetUrl = function (asset) {
+    const assetUrlParts = [
+        ASSET_SERVER,
+        'internalapi/asset/',
+        asset.assetId,
+        '.',
+        asset.dataFormat,
+        '/get/'
+    ];
+    return assetUrlParts.join('');
+};
+storage.addWebSource([storage.AssetType.Project], getProjectUrl);
+storage.addWebSource([storage.AssetType.ImageVector, storage.AssetType.ImageBitmap, storage.AssetType.Sound], getAssetUrl);
+vm.attachStorage(storage);
 
-{
-	vm.attachRenderer(renderer);
-}
 
-{
-    vm.attachAudioEngine(audioEngine);
-}
-//const audioEngine = new window.AudioEngine();
-//scratchVM.attachAudioEngine(audioEngine);
+//
+// Render Setting
+//
+vm.attachRenderer(renderer);
+
+
+//
+// Audio Setting
+//
+vm.attachAudioEngine(audioEngine);
+
+
+//
+// START & Download
+//
 vm.start();
 vm.downloadProjectId("119615668");
 
+//
+// Button Action Setting
+//
 startButton.onclick = function() {
 	vm.greenFlag();
 };
